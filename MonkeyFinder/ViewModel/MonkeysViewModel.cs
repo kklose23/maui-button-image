442d2345344ee7e10ff7de90d3eff6ae7d7a10e3
Kevin Klose <kevin.klose@teamdynamix.com>
2023-07-26 11:31:53 -0400

    first commit

---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+  public ObservableCollection<Monkey> Monkeys { get; } = new();
+  MonkeyService monkeyService;
+  IConnectivity connectivity;
+  IGeolocation geolocation;
+  public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
+  {
+    Title = "Monkey Finder";
+    this.monkeyService = monkeyService;
+    this.connectivity = connectivity;
+    this.geolocation = geolocation;
+  }
+
+  [ObservableProperty]
+  private bool showButton = false;
+
+  [RelayCommand]
+  public void ToggleButton()
+  {
+    ShowButton = !ShowButton;
+  }
+}
